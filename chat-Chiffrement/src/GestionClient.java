d92bc992384888f42c2dbdc8afb8ec0f1b290d2b
Etienne <etienne.choveau@hotmail.fr>
2021-01-28 15:42:44 +0100

    La créationd de la clé publique prend un argument

--- chat-Chiffrement/src/GestionClient.java
+++ chat-Chiffrement/src/GestionClient.java
@@ -8,21 +8,21 @@ public class GestionClient {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-
-    	ClePublique cp = new ClePublique();
-        ClePrive clePrive = new ClePrive(cp.getE(), cp.getM(), cp.getN());
-
-        System.out.println("Cle publique : "+cp.clePublique()[0]+"    "+cp.clePublique()[1]+"\n\n");
-        System.out.println("Cle privé : u : "+clePrive.getU()+"\n n : "+clePrive.getE());
-        
-        System.out.println();
-        System.out.println();
-
-        CryptographieRSA rsa = new CryptographieRSA();
-        System.out.println("Chiifrement / Dechiffrement RSA : " + rsa.dechiffrement(rsa.chiffrement("Bonjour Kevinoulle et Etienouille !!?", cp), clePrive));
-
         if (args.length == 1) {
               String monId = String.format( args[0]);
+
+              ClePublique cp = new ClePublique(monId);
+              ClePrive clePrive = new ClePrive(cp.getE(), cp.getM(), cp.getN());
+
+              System.out.println("Cle publique : "+cp.clePublique()[0]+"    "+cp.clePublique()[1]+"\n\n");
+              System.out.println("Cle privé : u : "+clePrive.getU()+"\n n : "+clePrive.getE());
+
+              System.out.println();
+              System.out.println();
+
+              CryptographieRSA rsa = new CryptographieRSA();
+              System.out.println("Chiifrement / Dechiffrement RSA : " + rsa.dechiffrement(rsa.chiffrement("Bonjour Kevinoulle et Etienouille !!?", cp), clePrive));
+
               ThreadClient client = new ThreadClient(monId);
               Thread t = new Thread(client);
               t.start();
