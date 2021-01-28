b4793b037cd081a6d6ad09aa769195c8fca75f6b
Etienne <etienne.choveau@hotmail.fr>
2021-01-28 12:21:32 +0100

    Ajout du jar

--- chat-Chiffrement/src/GestionClient.java
+++ chat-Chiffrement/src/GestionClient.java
@@ -9,24 +9,22 @@ public class GestionClient {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-
-    	ClePublique cp = new ClePublique();
         //ClePrive clePrive = new ClePrive(cp.getE(), cp.getM(), cp.getN());
-
-        System.out.println("Cle publique : "+cp.clePublique()[0]+"    "+cp.clePublique()[1]);
         //System.out.println(clePrive.getU());
         
         System.out.println();
         System.out.println();
 
+        /*
         Chiffrement ch = new Chiffrement();
         System.out.println(ch.chiffrementRSA("Bonjour !", cp));
-    	
+    	*/
+
         if (args.length == 1) {
               String monId = String.format( args[0]);
               ThreadClient client = new ThreadClient(monId);
               Thread t = new Thread(client);
-          t.start();
+              t.start();
         } else {
           System.out.println("syntaxe d’appel : java GestionClient nom_du_client\n");
         }
